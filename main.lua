0f72b2ad7b4f1121a274a247c78a600d12500921
VovaSv <vovasvidi@gmail.com>
2012-12-24 07:30:48 -0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+> t = { 3,2,5,1,4; n=3 }   -- construct a table with user size of 3
+> table.sort(t)            -- sort will be limited by user size
+> = table.concat(t, ", ")  -- only specified size is concatenated as well
